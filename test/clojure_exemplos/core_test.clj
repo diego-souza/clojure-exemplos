b99c3ec6e90cd37372b21164f6c04f37277a15a0
Diego Alves de Souza <diegosouza.mail@gmail.com>
2014-07-30 18:51:45 -0300

    Iniciado projeto do Leiningen.

---
+++ test/clojure_exemplos/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-exemplos.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-exemplos.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
